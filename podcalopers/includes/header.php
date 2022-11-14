3b1638856d3d51bd45d587decef3042ca459b50e
Enuar Lara <enuar2110@gmail.com>
2022-11-14 15:30:08 -0500

    Se crean includes

---
+++ podcalopers/includes/header.php
@@ -0,0 +1,5 @@
+<header class="header">
+    <a href="index.php">
+        <img class="header__logo" src="../../admin/files/logo/<?=$configuracion['conf_logo'];?>" alt="Logotipo">
+    </a>
+</header>
