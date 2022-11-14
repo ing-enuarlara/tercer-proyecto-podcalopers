7534929bf27c7ad5df992391c0466a3169823f95
Enuar Lara <enuar2110@gmail.com>
2022-11-14 15:29:39 -0500

    Se agregan includes

--- podcalopers/index.php
+++ podcalopers/index.php
@@ -1,42 +1,18 @@
 <?php
-include("conexion.php");
+include("includes/head.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>::FrontEnd Store::</title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
-    <header class="header">
-        <a href="index.php">
-            <img class="header__logo" src="../../admin/files/logo/<?=$configuracion['conf_logo'];?>" alt="Logotipo">
-
-        </a>
-
-    </header>
+    <?php
+    include("includes/header.php");
+    ?>
 
     <nav class="navegacion">
-        <a class="navegacion__enlace <?php if(!isset($_GET['cat'])){echo 'navegacion__enlace--activo';}?>" href="index.php">Tienda</a>
+        <a class="navegacion__enlace <?php if(!isset($_GET['cat'])){echo 'navegacion__enlace--activo';}?>" href="index.php">Productos</a>
         <?php
-        $activo='';
-        $cat='';
-        if(isset($_GET['cat'])){
-            $cat=$_GET['cat'];
-            $activo='navegacion__enlace--activo';
-        }
-        $categorias= $conexionBdComercial->query("SELECT * FROM comercial_categorias WHERE ccat_id_empresa=2");
-        while($result = mysqli_fetch_array($categorias, MYSQLI_BOTH)){
+        include("includes/navegacion-categorias.php");
         ?>
-        <a class="navegacion__enlace <?php if($result['ccat_id']==$cat){echo $activo;}?>" href="index.php?cat=<?=$result['ccat_id'];?>"><?=$result['ccat_nombre'];?></a>
-        <?php }?>
         <a class="navegacion__enlace" href="nosotros.php">Nosotros</a>
     </nav>
 
@@ -70,187 +46,14 @@ include("conexion.php");
                 </a>
             </div><!--producto-->
             <?php }?>
-
-            <div class="producto">
-                <a href="producto.php">
-                    <img class="producto__imagen" src="img/3.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">ReactJS</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/4.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">Redux</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/5.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">Nodes.js</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/6.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">SASS</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/7.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">HTML5</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/8.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">Github</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/9.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">BulmaCSS</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/10.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">TypeSccrip</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/11.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">Drupal</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/12.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">JavaScript</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/13.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">GraphQL</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
-            <div class="producto">
-                <a href="producto.html">
-                    <img class="producto__imagen" src="img/14.jpg" alt="imagen camisa">
-                    
-                    <div class="producto__informacion">
-                        <p class="producto__nombre">WordPress</p>
-                        <p class="producto__precio">$25</p>
-
-                    </div>
-
-                </a>
-
-            </div><!--producto-->
-
             <div class="grafico grafico--camisas"></div>
             <div class="grafico grafico--node"></div>
-
         </div>
-
     </main>
 
-    <footer class="footer">
-        <p class="footer__texto">Front End Store - Todos los derechos Reservados 2022.</p>
-
-    </footer>
-
+    <?php
+    include("includes/footer.php");
+    ?>
 </body>
 
 </html>
--- podcalopers/producto.php
+++ podcalopers/producto.php
@@ -1,56 +1,41 @@
 <?php
-include("conexion.php");
-$productos= $conexionBdComercial->query("SELECT * FROM comercial_productos WHERE cprod_id='".$_GET['id']."'");
-$resultadoP = mysqli_fetch_array($productos, MYSQLI_BOTH);
+include("includes/head.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>::FrontEnd Store::</title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
-    <header class="header">
-        <a href="index.php">
-            <img class="header__logo" src="../../admin/files/logo/<?=$configuracion['conf_logo'];?>" alt="Logotipo">
-        </a>
-
-    </header>
+    <?php
+    include("includes/header.php");
+    ?>
 
     <nav class="navegacion">
-        <a class="navegacion__enlace" href="index.php">Tienda</a>
-        <a class="navegacion__enlace navegacion__enlace--activo" href="#">Productos</a>
+        <a class="navegacion__enlace" href="index.php">Productos</a>
+        <?php
+        include("includes/navegacion-categorias.php");
+        ?>
+        <a class="navegacion__enlace" href="nosotros.php">Nosotros</a>
     </nav>
 
+    <?php
+        $productos= $conexionBdComercial->query("SELECT * FROM comercial_productos WHERE cprod_id='".$_GET['id']."'");
+        $resultadoP = mysqli_fetch_array($productos, MYSQLI_BOTH);
+    ?>
     <main class="contenedor">
         <h1><?=$resultadoP['cprod_nombre'];?></h1>
-
         <div class="camisa">
             <img src="../../admin/files/productos/<?=$resultadoP['cprod_foto'];?>" alt="imagen del producto" class="camisa__imagen">
-
             <div class="camisa__contenido">
                 <p><?=$resultadoP['cprod_detalles'];?>.</p>
                 <a href="https://api.whatsapp.com/send?phone=57<?=$configuracion['conf_telefono'];?>&text=hola, quiero adquirir el <?=$resultadoP['cprod_nombre'];?>." target="_blank">
                     <input class="producto__submit" type="submit" value="Comprar">
                 </a>
             </div>
-
         </div>
-
     </main>
 
-    <footer class="footer">
-        <p class="footer__texto">Front End Store - Todos los derechos Reservados 2022.</p>
-
-    </footer>
-
+    <?php
+    include("includes/footer.php");
+    ?>
 </body>
 
 </html>
