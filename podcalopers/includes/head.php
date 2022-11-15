a316450e88ae6bd2f336f5339ff0745823c5d129
Enuar Lara <enuar2110@gmail.com>
2022-11-14 22:32:33 -0500

    Se modifica URL de style

--- podcalopers/includes/head.php
+++ podcalopers/includes/head.php
@@ -8,7 +8,7 @@ include("conexion.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>::<?=$configuracion['conf_empresa'];?>::</title>
-    <link rel="stylesheet" href="css/normalize.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.php" type="text/css">
+    <link rel="stylesheet" href="css/normalize.css" type="text/css">
